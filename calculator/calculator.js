6a788e998c9885e43a141ed34d64bc622103eca4
tyler <tylercobb92@gmail.com>
2021-07-21 15:02:39 -0400

    removed calculator.copy.js to resolve errors when viewing page in firefox browser; starter code plus e.target click function copied to calculator.js instead

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -20,43 +20,25 @@ function getCurrentUIValues() {
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
-function setupInitialValues() {
-  const loanAmount = document.getElementById("loan-amount");
-  const loanYears = document.getElementById("loan-years");
-  const loanRate = document.getElementById("loan-rate");
-  loanAmount.value = 10000;
-  loanYears.value = 10;
-  loanRate.value = 0.1;
-  calculateMonthlyPayment();
-  updateMonthly();
+function setupIntialValues() {
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
-  const loanAmount = document.getElementById("loan-amount");
-  const loanYears = document.getElementById("loan-years");
-  const loanRate = document.getElementById("loan-rate");
-  loanAmount.textContent = loanAmount;
-  loanYears.textContent = loanYears;
-  loanRate.textContent = loanRate;
-  calculateMonthlyPayment();
-  updateMonthly();
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
-function calculateMonthlyPayment(P, n, i) {
-  P = document.getElementById("loan-amount").value;
-  n = (document.getElementById("loan-years").value) * 12;
-  i = (document.getElementById("loan-rate").value) / 12;
-  return ((P * i) / (1 - (1 + i) ** -(n))).toFixed(2);
+function calculateMonthlyPayment(values) {
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
-  monthly = document.getElementById("monthly-payment");
-  monthly.textContent = calculateMonthlyPayment();
 }
+
+document.addEventListener("click", function (e) {
+  console.log(e.target)
+})
